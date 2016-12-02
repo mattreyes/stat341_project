8eec052ca2e62603dcaf13ad22d4adc764012ec9
seyertam <matt.n.reyes@gmail.com>
2016-12-02 10:28:38 -0500

    add em to notebook

--- em.py
+++ em.py
@@ -1,14 +1,13 @@
 import math
 import numpy as np
-
-
+from matplotlib import pyplot as plt
 
 def em(data,num_iter=10000):
     mu1 = 0
     mu2 = 0
     sd1 = 1
     sd2 = 1
-    pi1 = 0.65
+    pi1 = 0.49
     pi2 = 1 - pi1
     n = len(data)
     
@@ -17,11 +16,14 @@ def em(data,num_iter=10000):
         dator = math.sqrt(2*math.pi)*sd
         return (nator/dator)
     def p1():
-        return prob(data,mu1,sd2)
+        return prob(data,mu1,sd1)
     def p2():
         return prob(data,mu2,sd2)
 
     for i in range(0,num_iter):
+        if i%500 == 0:
+            print('iteration ' + str(i))
+#         p1 = prob(data,mu1,sd2)
         w = (pi1*p1()) / (pi1*p1()+pi2*p2())
         n1 = np.sum(w)
         n2 = n - n1
@@ -31,23 +33,19 @@ def em(data,num_iter=10000):
         mu1 = (1/n1)*np.sum(w*data)
         mu2 = (1/n2)*np.sum((1-w)*data)
 
-        sd1 = math.sqrt(np.sum(w*(data-mu1)*(data-mu1))   )
+        sd1 = math.sqrt(np.sum(w*(data-mu1)*(data-mu1)) / n1   )
         sd2 = math.sqrt((1/n2)*np.sum((1-w)*(data-mu2)*(data-mu2)))
         
     print(str(mu1))    
     print(str(pi1))
     print(str(sd1))
-    print(str(mu2))
-    print(str(pi2))
-    print(str(sd2))
-    
+    print(str(mu2)) # 
+    print(str(pi2)) # did not change
+    print(str(sd2)) # correct
 
-
+np.random.seed(seed=420)
 x1 = np.random.normal(0,1,600)
 x2 = np.random.normal(6,3,400)
 x = np.append(x1,x2)
 
 em(x)
-
-
-
